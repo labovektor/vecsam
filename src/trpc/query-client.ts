4cb54c7fda5fb8f1c1e50f70e07e5a2073d7a1c7
anjarmath <anjarmath@gmail.com>
2025-04-21 14:54:50 +0700

    fix: query option

--- src/trpc/query-client.ts
+++ src/trpc/query-client.ts
@@ -8,10 +8,9 @@ export const createQueryClient = () =>
   new QueryClient({
     defaultOptions: {
       queries: {
-        // With SSR, we usually want to set some default staleTime
-        // above 0 to avoid refetching immediately on the client
-        staleTime: 2 * 60 * 1000, // 2 minutes
-        retry: 5,
+        refetchOnWindowFocus: false,
+        retry: 3,
+        staleTime: 2 * 60 * 1000,
       },
 
       dehydrate: {
