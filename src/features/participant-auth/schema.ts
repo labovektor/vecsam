b0dde94893b3a6f294c76da93b3223eb2fab668c
anjarmath <anjarmath@gmail.com>
2025-07-10 10:40:40 +0700

    extend participant passcode to 8 chars

--- src/features/participant-auth/schema.ts
+++ src/features/participant-auth/schema.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-  participantCode: z.string().length(6),
-  examCode: z.string().length(6),
+  participantCode: z.string().length(8, "Passcode harus berisi 8 karakter"),
+  examCode: z.string().length(6, "Passcode harus berisi 6 karakter"),
 });
 
 export type LoginSchemaType = z.infer<typeof loginSchema>;
